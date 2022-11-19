ad942bb9a03df78207b8c99d0970270976d04807
unknown <jashwanthbj9@gmail.com>
2022-11-20 01:09:38 +0530

    removed web sockets

--- src/helpers/socketHelper.js
+++ src/helpers/socketHelper.js
@@ -1,5 +1,4 @@
 import { io } from "socket.io-client";
 export const socket = io.connect(
-  "https://git.heroku.com/boiling-headland-42694.git",
-  { transports: ["websocket"] }
+  "https://git.heroku.com/boiling-headland-42694.git"
 );
