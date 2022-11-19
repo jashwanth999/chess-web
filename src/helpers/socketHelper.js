07af74cbe981d461db9aa9c6b2c6db41cdba2a66
unknown <jashwanthbj9@gmail.com>
2022-11-20 00:48:56 +0530

    changed transports to polling

--- src/helpers/socketHelper.js
+++ src/helpers/socketHelper.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 export const socket = io.connect(
   "https://git.heroku.com/boiling-headland-42694.git",
-  { transports: ["websocket"] }
+  { transports: ["polling"] }
 );
