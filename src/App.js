3829d6520017fab99ee3c473bab927b94cd47e38
unknown <jashwanthbj9@gmail.com>
2022-12-11 22:16:27 +0530

    added auto navigate to home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
 import ChessBoard from "./screens/ChessBoard";
 import Home from "./screens/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import WaitingScreen from "./screens/WaitingScreen";
 import LoginOrSignup from "./screens/LoginOrSignup";
 
 function App() {
+  const userId = localStorage.getItem("_id");
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginOrSignup />} />
+        <Route
+          path="/"
+          element={userId ? <Navigate to="/home" /> : <LoginOrSignup />}
+        />
         <Route path="/home" element={<Home />} />
         <Route path="/waiting" element={<WaitingScreen />} />
         <Route path="/room/:roomid" element={<ChessBoard />} />
